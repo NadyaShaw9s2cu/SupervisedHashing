0281e16b659b6f7c63ab8a26885aee40c3d1105f
NadyaShaw9s2cu <NadyaShaw9s2cu@gmail.com>
2016-04-11 22:41:50 +0800

    demo commit

--- demo.m
+++ demo.m
@@ -1,6 +1,8 @@
 % this program is used to compass data into binary code
 % a supervised hashing algorithm
 % 'fileName': .mat file, with a training matrix X and a label matrix label. Each row is a sample.
+
+% Just for fun, it's not original
 function demo(fileName)
 
 fprintf('Loading Dataset...\n')
@@ -11,8 +13,42 @@ dataset.X = normZeroMean(X);
 dataset.X = normEqualVariance(X);
 
 fprintf('Data partition\n')
-% test
+
+% reorder
 num_dataset = size( X, 1 );
+neworder = randperm(num_dataset);
+dataset.X = dataset.X(neworder);
+
+% partition
 num_test = 1000;
-test_id = randperm( num_dataset );
-dataset.X = dataset.X( test_id, : )
+mid = num_dataset - num_test;
+dataset.train_id = 1:mid;
+dataset.test_id = mid+1:num_dataset;
+dataset.neighbourType = 'label';
+dataset.label = dataset.label(neworder);
+
+% sparse
+dataset.X = effSparseCoding(dataset.X);
+
+% Parameter
+fprintf('Setting parameters...\n');
+method.learnImpl = 'stochastic';
+method.maxIter = 50;
+method.beta = 3e1;
+method.lambda = 1e0;
+codeLength = 64;
+
+% Training
+fprintf('Start training...\n')
+[BicodeTrain, BicodeTest, timeTrain, timeTest] = lfh(datset, method, codeLength);
+
+% Evaluation
+fprintf('Evaluating...\n')
+dataset.neighbourTest =calcNeighbour(dataset);
+neighbourH = calcHammingRank(BicodeTrain, BicodeTest);
+[MAP, succRate] = calcMAP(neighbourH, dataset.neighbourTest);
+
+fprintf('MAP: %f succRate:%f\n', MAP, succRate);
+fprintf('Train: %f(s)\n Test: %f(s)\n', timeTrain, timeTest);
+
+end
